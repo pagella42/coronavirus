1ee437d24820765af44631418bf9ea6e347d7dfb
pagella42 <pagella42@gmail.com>
2020-03-16 21:11:35 -0300

    fix

--- src/Menu.js
+++ src/Menu.js
@@ -11,7 +11,10 @@ class Menu extends Component {
             <div className="circle-cont">
               <div className="button-cont-1 circle-small">
                 <img style={{ width: "12vw", height: "12vw" }} src={require("./components/info.png")} />
+               <div class="menu-text-box">
                 <span className="menu-text">¿Que es?</span>
+
+               </div>
               </div>
 
             </div>
@@ -19,16 +22,20 @@ class Menu extends Component {
             <div className="circle-cont">
               <div className="button-cont-3 circle-big" onClick={()=>this.props.changePage("menu", "test")}>
                 <img style={{ width: "17vw", height: "17vw" }} src={require("./components/test.png")} />
-
+                <div class="menu-text-box">
                 <span className="menu-text">Realizar test</span>
+
+                </div>
               </div>
             </div>
 
             <div className="circle-cont">
               <div className="button-cont-2 circle-small">
                 <img style={{ width: "12vw", height: "12vw" }} src={require("./components/prevencion.png")} />
+                <div class="menu-text-box">
 
                 <span className="menu-text">¿Como prevenir?</span>
+                </div>
               </div>
 
             </div>
