9883ed31d6aa176146316f0eb228346a8ebb6a0f
pagella42 <pagella42@gmail.com>
2020-03-18 19:49:13 -0300

    https

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require ('express')
+const bodyParser = require ('body-parser')
+var sslRedirect = require('heroku-ssl-redirect');
+
+const path = require('path')
+const app = express()
+const PORT = 4000
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+
+    next()
+})
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
+app.use(sslRedirect());
+
+
+
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || PORT,()=>console.log(`Running on port: ${PORT}`))
