d90a796422f312c04c3fd21cec54398607fed397
pagella42 <pagella42@gmail.com>
2020-03-19 12:58:59 -0300

    share

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import './Main.css';
-import { Link } from "react-router-dom";
-
+import {
+    FacebookShareButton,
+    LinkedinShareButton,
+    TwitterShareButton,
+    WhatsappShareButton,
+  } from "react-share";
 class Footer extends Component {
 
     render() {
         return (
                 <div id="footer">
                     {/* <span className="sponsors-text">SPONSORS</span> */}
-    
-                    <div className="footer-ref-cont">
+                    <FacebookShareButton url={"https://www.test-coronavirus.com.ar/"} />
+                    {/* <div className="footer-ref-cont">
                         <span className="footer-ref">
                     *Referencia: Organización Mundial de la Salud (OMS) - <a className="footer-link" href="https://www.who.int/health-topics/coronavirus">Ref 1</a> / <a className="footer-link" href="https://www.who.int/news-room/q-a-detail/q-a-coronaviruses">Ref 2</a>
                     </span>
@@ -17,7 +21,7 @@ class Footer extends Component {
                     <span className="footer-ref">
                     * Referencia: <a className="footer-link" href="https://www.buenosaires.gob.ar/coronavirus">Protocolo Gobierno de la Ciudad de Buenos Aires </a> 
                     </span> 
-                    </div>
+                    </div> */}
                    
                 </div>
         )
