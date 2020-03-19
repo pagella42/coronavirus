9aef0313542e622f2ca6f7f8af500165ea08b14c
pagella42 <pagella42@gmail.com>
2020-03-19 15:11:35 -0300

    minify

--- src/Contact.js
+++ src/Contact.js
@@ -18,68 +18,7 @@ class Contact extends Component {
 
         return (
 
-            <div id="body-cont">
-
-                <div id="card">
-
-                    <div id="question-box">
-                        <span className="question-text-contact">¿Has tenido contacto estrecho con alguna
-                        persona que sea un caso en estudio o
-                                confirmado de coronavirus SARS-CoV-2?</span>
-                    </div>
-
-                    <div id="card-body-contact">
-
-                        <div id="contact-options">
-                            <span>
-                                •Contacto estrecho significa alguna de estas situaciones:
-                              </span>
-                            <span>
-                                •Vivir en la misma vivienda
-                            </span>
-                            <span>
-                                •Tener contacto cara a cara. Ejemplo: una conversación de algunos minutos
-                            </span>
-                            <span>
-                                •Si ha tosido cerca de usted
-                            </span>
-                            <span>
-                                •Estar a menos de 2 metros de la persona durante más de 15 minutos
-                            </span>
-                            <span>
-                                •Estar en contacto con sus secreciones (mucosidad, saliva, heces, orina, sangre,
-                                vómitos, etc.)
-                            </span>
-
-                        </div>
-                        <div id="travel-box">
-                            <div className="symptom-row">
-                                <div id={this.state.contacted === true ? "selected-button" : "select-button"} onClick={() => this.contacted(true)} >
-                                    <span>{this.state.contacted === true ? "-" : "+"} </span>
-                                </div>
-                                <span>Si</span>
-                            </div>
-
-                            <div className="symptom-row">
-                                <div id={this.state.contacted === false ? "selected-button" : "select-button"} onClick={() => this.contacted(false)} >
-                                    <span>{this.state.contacted === false ? "-" : '+'}</span>
-                                </div>
-                                <span>No</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div id="card-button-cont">
-                        <div id="card-button" onClick={() => this.state.contacted !== null ? this.props.moveOn("showContacted", "showDisclaimer", this.state.contacted, "contacted") : null}>
-                            <span>
-                                Siguiente
-                           </span>
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
+            <div id="body-cont"> <div id="card"> <div id="question-box"> <span className="question-text-contact">¿Has tenido contacto estrecho con alguna persona que sea un caso en estudio o confirmado de coronavirus SARS-CoV-2?</span> </div><div id="card-body-contact"> <div id="contact-options"> <span> •Contacto estrecho significa alguna de estas situaciones: </span> <span> •Vivir en la misma vivienda </span> <span> •Tener contacto cara a cara. Ejemplo: una conversación de algunos minutos </span> <span> •Si ha tosido cerca de usted </span> <span> •Estar a menos de 2 metros de la persona durante más de 15 minutos </span> <span> •Estar en contacto con sus secreciones (mucosidad, saliva, heces, orina, sangre, vómitos, etc.) </span> </div><div id="travel-box"> <div className="symptom-row"> <div id={this.state.contacted===true ? "selected-button" : "select-button"}onClick={()=> this.contacted(true)}> <span>{this.state.contacted===true ? "-" : "+"}</span> </div><span>Si</span> </div><div className="symptom-row"> <div id={this.state.contacted===false ? "selected-button" : "select-button"}onClick={()=> this.contacted(false)}> <span>{this.state.contacted===false ? "-" : '+'}</span> </div><span>No</span> </div></div></div><div id="card-button-cont"> <div id="card-button" onClick={()=> this.state.contacted !==null ? this.props.moveOn("showContacted", "showDisclaimer", this.state.contacted, "contacted") : null}> <span> Siguiente </span> </div></div></div></div>
 
         )
     }
--- src/Symptoms.js
+++ src/Symptoms.js
@@ -25,67 +25,7 @@ this.setState({[value]: data})
       malestar:this.state.malestar}
     return (
 
-      <div id="body-cont">
-
-        <div id="card">
-          <div id="question-box">
-
-            <span className="question-text">Selecciona si tenes alguno de estos sintomas</span>
-          </div>
-          <div id="card-body">
-
-            <div id="symptoms-box">
-              <div className="symptom-row">
-                <div id={this.state.fiebre ? "selected-button" : "select-button"} onClick={()=>this.loadValue("fiebre",!this.state.fiebre)}>
-                  <span>
-                  {this.state.fiebre ? "-" : "+"}
-                  </span>
-                </div>
-                <span>Fiebre</span>
-              </div>
-
-              <div className="symptom-row">
-                <div id={this.state.tos ? "selected-button" : "select-button"} onClick={()=>this.loadValue("tos",!this.state.tos)} >
-                  <span>
-                  {this.state.tos ? "-" : "+"}
-              </span>
-                </div>
-                <span>Tos</span>
-              </div>
-
-              <div className="symptom-row">
-                <div id={this.state.respiratorio ? "selected-button" : "select-button"} onClick={()=>this.loadValue("respiratorio",!this.state.respiratorio)} >
-                  <span>
-                  {this.state.respiratorio ? "-" : "+"}
-              </span>
-                </div>
-                <span>Dificultad respiratoria</span>
-              </div>
-
-              <div className="symptom-row">
-                <div id={this.state.malestar ? "selected-button" : "select-button"} onClick={()=>this.loadValue("malestar",!this.state.malestar)} >
-                  <span>
-                  {this.state.malestar ? "-" : "+"}
-              </span>
-                </div>
-                <span>Malestar general</span>
-              </div>
-            </div>
-
-          </div>
-          <div id="card-button-cont">
-  
-            <div id="card-button" onClick={()=>this.props.moveOn("showSymptoms","showTraveled",data, "symptoms")}>
-              <span>
-                Siguiente
-              </span>
-            </div>
-            
-          </div>
-
-        </div>
-
-      </div>
+      <div id="body-cont"> <div id="card"> <div id="question-box"> <span className="question-text">Selecciona si tenes alguno de estos sintomas</span> </div><div id="card-body"> <div id="symptoms-box"> <div className="symptom-row"> <div id={this.state.fiebre ? "selected-button" : "select-button"}onClick={()=>this.loadValue("fiebre",!this.state.fiebre)}> <span>{this.state.fiebre ? "-" : "+"}</span> </div><span>Fiebre</span> </div><div className="symptom-row"> <div id={this.state.tos ? "selected-button" : "select-button"}onClick={()=>this.loadValue("tos",!this.state.tos)}> <span>{this.state.tos ? "-" : "+"}</span> </div><span>Tos</span> </div><div className="symptom-row"> <div id={this.state.respiratorio ? "selected-button" : "select-button"}onClick={()=>this.loadValue("respiratorio",!this.state.respiratorio)}> <span>{this.state.respiratorio ? "-" : "+"}</span> </div><span>Dificultad respiratoria</span> </div><div className="symptom-row"> <div id={this.state.malestar ? "selected-button" : "select-button"}onClick={()=>this.loadValue("malestar",!this.state.malestar)}> <span>{this.state.malestar ? "-" : "+"}</span> </div><span>Malestar general</span> </div></div></div><div id="card-button-cont"> <div id="card-button" onClick={()=>this.props.moveOn("showSymptoms","showTraveled",data, "symptoms")}> <span> Siguiente </span> </div></div></div></div>
 
     )
   }
